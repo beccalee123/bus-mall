f420a342de178288d3a60b6f29293220c215bc94
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2018-10-22 15:25:09 -0700

    sets up randomizer for 3 images

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ new BusMallItem('Open-Toed Rainboots', 'img/boots.jpg');
 new BusMallItem('Full Breakfast Appliance', 'img/breakfast.jpg');
 new BusMallItem('Meatball Bubblegum', 'img/bubblegum.jpg');
 new BusMallItem('Extremely Comfortable Chair', 'img/chair.jpg');
-new BusMallItem('Cthulu Figurine', 'img/cthulu.jpg');
+new BusMallItem('Cthulhu Figurine', 'img/cthulhu.jpg');
 new BusMallItem('Duck Bill Dog Muzzle', 'img/dog-duck.jpg');
 new BusMallItem('Dragon Meat', 'img/dragon.jpg');
 new BusMallItem('Pen Utensils', 'img/pen.jpg');
@@ -36,19 +36,50 @@ new BusMallItem('Extremely Easy to Drink From Wine Glass', 'img/wine-glass.jpg')
 
 
 // Get the <img> element from the DOM
-var itemImage = document.getElementById('busmall-items');
+var itemImage1 = document.getElementById('busmall-item-1');
+var itemImage2 = document.getElementById('busmall-item-2');
+var itemImage3 = document.getElementById('busmall-item-3');
 
-// Write a function to randomly display one of the images
-function randomItem() {
+
+// Write a function to randomly display 1st image
+function randomItem1() {
   // Select a random item from the array
   var idx = Math.floor(Math.random() * allBusMallItems.length);
   console.log(allBusMallItems[idx]);
   // Assign the src, alt, and title attributes to the <img> element
-  itemImage.src = allBusMallItems[idx].filepath;
-  itemImage.alt = allBusMallItems[idx].name;
-  itemImage.title = allBusMallItems[idx].name;
+  itemImage1.src = allBusMallItems[idx].filepath;
+  itemImage1.alt = allBusMallItems[idx].name;
+  itemImage1.title = allBusMallItems[idx].name;
   // Console log which item is showing
   console.log(`${allBusMallItems[idx].name} is showing`);
 }
 
-randomItem();
+// Write a function to randomly display 2nd image
+function randomItem2() {
+  // Select a random item from the array
+  var idx = Math.floor(Math.random() * allBusMallItems.length);
+  console.log(allBusMallItems[idx]);
+  // Assign the src, alt, and title attributes to the <img> element
+  itemImage2.src = allBusMallItems[idx].filepath;
+  itemImage2.alt = allBusMallItems[idx].name;
+  itemImage2.title = allBusMallItems[idx].name;
+  // Console log which item is showing
+  console.log(`${allBusMallItems[idx].name} is showing`);
+}
+
+// Write a function to randomly display 3rd image
+function randomItem3() {
+  // Select a random item from the array
+  var idx = Math.floor(Math.random() * allBusMallItems.length);
+  console.log(allBusMallItems[idx]);
+  // Assign the src, alt, and title attributes to the <img> element
+  itemImage3.src = allBusMallItems[idx].filepath;
+  itemImage3.alt = allBusMallItems[idx].name;
+  itemImage3.title = allBusMallItems[idx].name;
+  // Console log which item is showing
+  console.log(`${allBusMallItems[idx].name} is showing`);
+}
+
+randomItem1();
+randomItem2();
+randomItem3();
